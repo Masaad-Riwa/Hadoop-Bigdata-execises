b4af97aa079052ebe5016e7c37b109f11813cd34
Masaad-Riwa <riwa.masaad@epita.fr>
2024-04-28 14:04:35 +0000

    ex5

--- MapReduceBasicProject/src/main/java/epita/fr/Exercise3/MapperBigData.java
+++ MapReduceBasicProject/src/main/java/epita/fr/Exercise3/MapperBigData.java
@@ -2,7 +2,6 @@ package epita.fr.Exercise3;
 
 import java.io.IOException;
 import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
