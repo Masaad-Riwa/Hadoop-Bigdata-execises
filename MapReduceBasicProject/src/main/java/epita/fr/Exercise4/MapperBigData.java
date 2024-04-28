f419d5afdc40e54355aed8879ea6aefc0e250eb5
Masaad-Riwa <riwa.masaad@epita.fr>
2024-04-28 18:43:41 +0000

    ex4 with errors

--- MapReduceBasicProject/src/main/java/epita/fr/Exercise4/MapperBigData.java
+++ MapReduceBasicProject/src/main/java/epita/fr/Exercise4/MapperBigData.java
@@ -14,21 +14,31 @@ class MapperBigData extends Mapper<LongWritable, Text, Text, Text> {
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         // Split the input line into fields: zoneId, date, pm10Value
-        String[] fields = value.toString().split("\\s*,\\s*");
+        
+		//String[] fields = value.toString().split("\\s*,\\s*");
 
         // Extract zoneId, date, and pm10Value from the fields
-        String zoneId = fields[0];
-        String date = fields[1];
-        double pm10Value = Double.parseDouble(fields[2]);
+        // String zoneId = fields[0];
+        // String date = fields[1];
+        // double pm10Value = Double.parseDouble(fields[2]);
+		try{
+		System.out.println(value.toString());
+		String[] parts = value.toString().split("\\t");
+		System.out.println("parts" + parts);
+        String zoneId = parts[0].split(",")[0];
+		String date = parts[0].split(",")[1];
+        double pm10Value = Double.parseDouble(parts[1]);
 
+		System.out.println(pm10Value + " " + date);
         // Emit key-value pair with zoneId as key and date as value
-        try{
+
 		if (pm10Value > 50.0) { // Check if PM10 value exceeds threshold
             context.write(new Text(zoneId), new Text(date));
-        }
-		} catch (Exception e) {
+			System.out.println("aaaaa");
+		}
+	 } catch (Exception e) {
 		System.err.println("Error during context.write: " + e.getMessage());
 		e.printStackTrace(); // This will log the stack trace
-	}
+		}
     }
 }
