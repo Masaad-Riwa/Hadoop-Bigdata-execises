8818828f5bc7dd8a5c27c94f258e8bd9af4bd08e
Masaad-Riwa <riwa.masaad@epita.fr>
2024-05-02 15:46:19 +0000

    fixes

--- MapReduceBasicProject/src/main/java/epita/fr/Exercise4/MapperBigData.java
+++ MapReduceBasicProject/src/main/java/epita/fr/Exercise4/MapperBigData.java
@@ -13,14 +13,6 @@ class MapperBigData extends Mapper<LongWritable, Text, Text, Text> {
 
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        // Split the input line into fields: zoneId, date, pm10Value
-        
-		//String[] fields = value.toString().split("\\s*,\\s*");
-
-        // Extract zoneId, date, and pm10Value from the fields
-        // String zoneId = fields[0];
-        // String date = fields[1];
-        // double pm10Value = Double.parseDouble(fields[2]);
 		try{
 		System.out.println(value.toString());
 		String[] parts = value.toString().split("\\t");
@@ -31,14 +23,13 @@ class MapperBigData extends Mapper<LongWritable, Text, Text, Text> {
 
 		System.out.println(pm10Value + " " + date);
         // Emit key-value pair with zoneId as key and date as value
-
 		if (pm10Value > 50.0) { // Check if PM10 value exceeds threshold
             context.write(new Text(zoneId), new Text(date));
 			System.out.println("aaaaa");
 		}
 	 } catch (Exception e) {
 		System.err.println("Error during context.write: " + e.getMessage());
-		e.printStackTrace(); // This will log the stack trace
+		e.printStackTrace(); 
 		}
     }
 }
